82d001e550aa2aa0cd32e9a046d65f02c48c084c
Eoin Crosbie <eoin.crosbie@feedhenry.com>
2012-12-07 16:24:50 +0000

    Create localStorage module

--- client/default/app/models/Store.js
+++ client/default/app/models/Store.js
@@ -14,9 +14,10 @@ define(['zepto',
 	//interface----------------------------------
 	Peachy.Models.Store = {
 
-		save : _save, 	// save a model to local storage
-		load : _load,	// load a model from local storage
-		clear: _clear 	// clear all local storage
+		save	: _save, 	// save a model to local storage
+		load	: _load,	// load a model from local storage
+		clear	: _clear 	// clear a model from local storage
+		clearAll: _clearAll // wipe local storage
 	};
 
 	//implementation-------------------------------
@@ -24,8 +25,43 @@ define(['zepto',
 	function _save(modelName, model, callback){
 		var model = model || {};
 
-		
-	}
+		$fh.data({
+			act : 'save',
+			key : modelName,
+			val : model
+		}, function(){
+			return callback(true);
+		}, function(msg, err){
+			return callback(msg, err);
+		});
+	};
+
+	function _load(modelName, callback){
+
+		$fh.data({
+			key : modelName
+		}, function(res){
+			return callback(true, res);
+		}, function(msg, err){
+			return callback(msg, err);
+		});
+	};
+
+	function _clear(modelName, callback){
+
+		$fh.data({
+			act : 'remove',
+			key : modelName
+		}, function(){
+			return callback(true);
+		}, function(msg, err){
+			return callback(msg, err);
+		});
+	};
+
+	function _clearAll(callback){
+		// TODO -- write clear all function to wipe storage
+	};
 
 	return Peachy.Models.Store;
 
