0251745036c8ea4fb03bc54b123d5d0765909e4c
Ashish Bhargava <ashish.bhargava@mindstix.com>
2013-01-24 12:41:24 +0530

    Cloud code for quotes FIXED

--- cloud/endpoints/fetchQuotes.js
+++ cloud/endpoints/fetchQuotes.js
@@ -3,7 +3,6 @@
  * 
  */
 var http = require('http');
-var sessionManager = require('../lib/session/session.js');
 var appConfig = require('../config/appConfig.js')
 var jsonUtils = require('../lib/jsonUtils.js');
 var log = require('../lib/log/log.js');
@@ -17,93 +16,71 @@ var fetchQuotesEndpoint = function() {
      */
     // Exposed operations
     this.fetchQuotes = function fetchQuotes(reqJson, callback){
-        if (jsonUtils.getPath(reqJson, "request.head.sessionId") == null)         
+        if (reqJson == null)
         {
-            log.error("[fetchQuotesEndpoint][fetchQuotes] >> SessionId Not Available");
-            var responseJson = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_AUTH_FAILED, "Authentication  Fail",{});
-            return callback(responseJson,null) 
+            var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SERVER_ERROR, "Internal server error",{});
+            return  callback(fail,null) 
         }
-        // Extract sessionId from request params
-        var sessionId = jsonUtils.getPath(reqJson, "request.head.sessionId").trim();
-        
-        //Fetching session details
-        sessionManager.getSession(sessionId, function(err, data ){
-            log.info("[fetchQuotesEndpoint][fetchQuotes] >> Session Details :"+JSON.stringify(data));
-            if(data)
-            {
-                //Setting the apiSession to fetch the Quotes details      
-                var apiSessionId = data.apiSessionId;
-        
-                // preparing the header
-                var getHeaders = {
-                    'Content-Type' : 'application/json',
-                    'sessionId' : apiSessionId
-                };
-                var env = appConfig.current;
+        var getHeaders = {
+            'Content-Type' : 'application/json'
+        };
+        var env = appConfig.current;
   
-                // perparing the GET options
-                var optionsGet = {
-                    host : appConfig.environments[env].urls.baseUrl,
-                    port : appConfig.environments[env].urls.port,
-                    path : appConfig.environments[env].urls.quotes,
-                    method : 'GET',
-                    headers : getHeaders
-                };
+        // perparing the GET options
+        var optionsGet = {
+            host : appConfig.environments[env].urls.baseUrl,
+            port : appConfig.environments[env].urls.port,
+            path : appConfig.environments[env].urls.quotes,
+            method : 'GET',
+            headers : getHeaders
+        };
 
-                var reqGet = http.request(optionsGet, function(res) {
-                     if (res.statusCode == 403)
-                    {
-                         var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_AUTH_FAILED, "Authentication  Fail",{});
-                        return  callback(fail,null) 
-                    }
-                    else if (res.statusCode == 500)
-                    {
-                        var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SERVER_ERROR, "Internal server error",{});
-                        return  callback(fail,null) 
-                    }
-                    else if (res.statusCode == 200)
-                    {                    
-                        console.log("statusCode: "+ res.statusCode);
-                        var data="";
-                        res.on('data', function(d) {
-                            //fetching the complete response that comes in chunks in 'data'
-                            data+=d; 
-                        });
-                        res.on('end',function(){
-                            if(data)
-                            {
-                                var jsonObject;
-                                //converting the response data into JSON object
-                                jsonObject= JSON.parse(data.toString());
-                                var jsonObj = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SUCCESS, "fetchQuotes Success",jsonObject);
-                                return callback(null,jsonObj) //callback returning the response JSON back to client 
-                            }
-                            else        //if complete data not found
-                            {
-                                var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SERVER_ERROR, "Internal server error",{});
-                                // Error ...!!!
-                                return  callback(fail,null) 
-                            }
-                        })
-                    }
-                    else               //if GET call is not successful  
-                    {
-                        var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SERVER_ERROR, "Internal server error",{});
-                        return  callback(fail,null) 
-                    }
-                });
- 
-                reqGet.end();
-                reqGet.on('error', function(e) {
-                    log.error("[fetchQuotesEndpoint][fetchQuotes][regGet] >> " + e);
-                    return  callback(fail,null)
-                });
-            } 
-            else        //If session not found
+        var reqGet = http.request(optionsGet, function(res) {
+            if (res.statusCode == 403)
             {
-                var responseJson = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_AUTH_FAILED, "Authentication  Fail",{});
-                return callback(responseJson,null) 
+                var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_AUTH_FAILED, "Authentication  Fail",{});
+                return  callback(fail,null) 
             }
+            else if (res.statusCode == 500)
+            {
+                var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SERVER_ERROR, "Internal server error",{});
+                return  callback(fail,null) 
+            }
+            else if (res.statusCode == 200)
+            {                    
+                console.log("statusCode: "+ res.statusCode);
+                var data="";
+                res.on('data', function(d) {
+                    //fetching the complete response that comes in chunks in 'data'
+                    data+=d; 
+                });
+                res.on('end',function(){
+                    if(data)
+                    {
+                        var jsonObject;
+                        //converting the response data into JSON object
+                        jsonObject= JSON.parse(data.toString());
+                        var jsonObj = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SUCCESS, "fetchQuotes Success",jsonObject);
+                        return callback(null,jsonObj) //callback returning the response JSON back to client 
+                    }
+                    else        //if complete data not found
+                    {
+                        var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SERVER_ERROR, "Internal server error",{});
+                         return  callback(fail,null) 
+                    }
+                })
+            }
+            else               //if GET call is not successful  
+            {
+                var fail = respUtils.constructStatusResponse("fetchQuotes", constants.RESP_SERVER_ERROR, "Internal server error",{});
+                return  callback(fail,null) 
+            }
+        });
+ 
+        reqGet.end();
+        reqGet.on('error', function(e) {
+            log.error("[fetchQuotesEndpoint][fetchQuotes][regGet] >> " + e);
+            return  callback(fail,null)
         });           
     }
 }
