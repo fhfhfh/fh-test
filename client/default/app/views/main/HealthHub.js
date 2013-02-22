d59242f67b59072e0cfea87b8647994231e01f77
Ashish Bhargava <ashish.bhargava@mindstix.com>
2013-02-22 18:35:59 +0530

    HealthHub endpoints integrated with app

--- client/default/app/views/main/HealthHub.js
+++ client/default/app/views/main/HealthHub.js
@@ -1,74 +1,118 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'views/ContainerView',
-  'views/main/healthHub/History',
-  'views/main/healthHub/Data',
-  'views/main/healthHub/Conditions',
-  'text!templates/pages/HealthHub.html'
-], function($, _, Backbone, ContainerView, HistoryView, DataView, ConditionsView, template) {
+    'jquery',
+    'underscore',
+    'backbone',
+    'views/ContainerView',
+    'views/main/healthHub/History',
+    'views/main/healthHub/Data',
+    'views/main/healthHub/Conditions',
+    'text!templates/pages/HealthHub.html',
+    'controllers/HealthHub',
+    ], function($, _, Backbone, ContainerView, HistoryView, DataView, ConditionsView, template,controller) {
+        var resp = "";
+        return ContainerView.extend({
+            tagName	: 'section',
+            id		: 'healthHub',
+            //            resp        : "",
 
-  return ContainerView.extend({
-    tagName	: 'section',
-    id		: 'healthHub',
+            events : {
+                'click #show-history'		: 'showHistory',
+                'click #show-data'			: 'showData',
+                'click #show-conditions'	: 'showConditions'
+            },
 
-    events : {
-      'click #show-history'		: 'showHistory',
-      'click #show-data'			: 'showData',
-      'click #show-conditions'	: 'showConditions'
-    },
+            subViews: {
+                history	: new HistoryView(),
+                data		: new DataView(),
+                conditions: new ConditionsView()
+            },
 
-    subViews: {
-      history	: new HistoryView(),
-      data		: new DataView(),
-      conditions: new ConditionsView()
-    },
+            initialize: function(options) {
+                var self = this;
+                _.bindAll(this);
+                
+                this.$el.html(template);
+                this.$content = this.$('#healthHub-content');
+            },
 
-    initialize: function(options) {
-      var self = this;
-      _.bindAll(this);
+            render: function() {
+                
+                var self = this;
+                this.setActiveView('data');
+                this.delegateEvents();
+                if (this.activeView) {
+                    this.activeView.delegateEvents();
+                // this.setActiveView(self.activeView);
+                }
+                setValue();
+                this.$('li').removeClass('selected');
+                this.$('#show-data').addClass('selected');
+                
+                return this;
+            },
 
-      this.$el.html(template);
-      this.$content = this.$('#healthHub-content');
-    },
+            refreshScroll: function(){
+                if(this.container){
+                    this.container.refreshScroll();  
+                }
+            },
 
-    render: function() {
-      var self = this;
-      this.setActiveView('data');
-      this.delegateEvents();
-      if (this.activeView) {
-        this.activeView.delegateEvents();
-        // this.setActiveView(self.activeView);
-      }
-      this.$('li').removeClass('selected');
-      this.$('#show-data').addClass('selected');
-      return this;
-    },
+            showHistory : function(){
+                this.$('li').removeClass('selected');
+                this.$('#show-history').addClass('selected');
+                this.setActiveView('history');
+                for(var i=0; i<resp.payload.familyHistories.length; i++)
+                    $('div #history #familyHistory #body').append('<div id="row"><span class="name">'+resp.payload.familyHistories[i].familyMember+'</span><span class="value">'+resp.payload.familyHistories[i].diagnosis+'</span></div>');
+                for(var i=0; i<resp.payload.familyHistories.length; i++)
+                    $('div #history #socialHistory #body').append('<div id="row"><span class="name">'+resp.payload.socialHistories[i].socialHistoryElement+'</span><span class="value">'+resp.payload.socialHistories[i].description+'</span></div>');
+            },
 
-    refreshScroll: function(){
-      if(this.container){
-        this.container.refreshScroll();  
-      }
-    },
+            showData : function(){
+                this.$('li').removeClass('selected');
+                this.$('#show-data').addClass('selected');
+                this.setActiveView('data');
+                for(var i=0; i<resp.payload.results.length; i++)
+                    $('div #data #testResults #body').append('<div id="row"><span class="name">'+resp.payload.results[i].testName+'</span><span class="value">'+resp.payload.results[i].result+' '+resp.payload.results[i].units+'</span></div>');
+                for(var i=0; i<resp.payload.immunizations.length; i++)
+                    $('div #data #immunizations #body').append('<div id="row"><span class="name">'+resp.payload.immunizations[i].vaccine+'</span><span class="value">'+resp.payload.immunizations[i].status+'</span></div>');
+                
+            },
 
-    showHistory : function(){
-      this.$('li').removeClass('selected');
-      this.$('#show-history').addClass('selected');
-      this.setActiveView('history');
-    },
+            showConditions : function(){
+                this.$('li').removeClass('selected');
+                this.$('#show-conditions').addClass('selected');
+                this.setActiveView('conditions');
+                for(var i=0; i<resp.payload.problems.length; i++)
+                    $('div #conditions #problems #body').append('<div id="row"><span class="name">'+resp.payload.problems[i].problem+'</span><span class="value">'+resp.payload.problems[i].status+'</span></div>');
+                for(var i=0; i<resp.payload.procedures.length; i++)
+                    $('div #conditions #procedures #body').append('<div id="row"><span class="name">'+resp.payload.procedures[i].procedure+'</span><span class="value">'+resp.payload.procedures[i].description+'</span></div>');
+                for(var i=0; i<resp.payload.allergies.length; i++)
+                    $('div #conditions #allergies #body').append('<div id="row"><span class="name">'+resp.payload.allergies[i].allergry+'</span><span class="value">'+resp.payload.allergies[i].reaction+'</span></div>');
+                for(var i=0; i<resp.payload.encounters.length; i++)
+                    $('div #conditions #recentVisits #body').append('<div id="row"><span class="name">'+resp.payload.encounters[i].encounter+'</span><span class="value">'+resp.payload.encounters[i].location+'</span></div>');
+                
+            }
 
-    showData : function(){
-      this.$('li').removeClass('selected');
-      this.$('#show-data').addClass('selected');
-      this.setActiveView('data');
-    },
-
-    showConditions : function(){
-      this.$('li').removeClass('selected');
-      this.$('#show-conditions').addClass('selected');
-      this.setActiveView('conditions');
-    }
-
-  });
-});
+        });
+        function setValue (){
+            if (resp == "")
+                controller.loadHealthHub(function (err, res){
+                    if(res || resp == ""){
+                        resp = res;
+                        for(var i=0; i<resp.payload.results.length; i++)
+                            $('div #data #testResults #body').append('<div id="row"><span class="name">'+resp.payload.results[i].testName+'</span><span class="value">'+resp.payload.results[i].result+' '+resp.payload.results[i].units+'</span></div>');
+                        for(var i=0; i<resp.payload.immunizations.length; i++)
+                            $('div #data #immunizations #body').append('<div id="row"><span class="name">'+resp.payload.immunizations[i].vaccine+'</span><span class="value">'+resp.payload.immunizations[i].status+'</span></div>');
+                        return;
+                    }
+                })
+            else
+            {
+                for(var i=0; i<resp.payload.results.length; i++)
+                    $('div #data #testResults #body').append('<div id="row"><span class="name">'+resp.payload.results[i].testName+'</span><span class="value">'+resp.payload.results[i].result+' '+resp.payload.results[i].units+'</span></div>');
+                for(var i=0; i<resp.payload.immunizations.length; i++)
+                    $('div #data #immunizations #body').append('<div id="row"><span class="name">'+resp.payload.immunizations[i].vaccine+'</span><span class="value">'+resp.payload.immunizations[i].status+'</span></div>');
+                return;
+            }
+        };
+    });
