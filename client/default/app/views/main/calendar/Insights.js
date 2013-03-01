5273a81172e9755ab1eb4616bf63182492360313
Ashish Bhargava <ashish.bhargava@mindstix.com>
2013-03-01 18:53:27 +0530

    started with insight view

--- client/default/app/views/main/calendar/Insights.js
+++ client/default/app/views/main/calendar/Insights.js
@@ -15,11 +15,22 @@ define(['jquery',
 	    id			: 'insights',
 	    events		: {
 	    },
+            monthName: [
+            'January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+            'October', 'November', 'December'
+            ],
+            monthDays:['Sunday','Monday','Tuesday','wednesday','Thursday','Friday','Saturday'],
 	    template	: _.template(tpl),
 
 
 		initialize : function(){
 			_.bindAll(this);
+                        var date		= new Date();
+                this.date		= date;
+                this.month		= date.getMonth();
+                this.year		= date.getFullYear();
+                this.day                = date.getDay();
+                this.dayWeek            = this.monthDays[date.getUTCDay()];
 			this.date = new Date().toString('dddd, MMMM ,yyyy');
 		},
 
@@ -29,8 +40,7 @@ define(['jquery',
 			this.$el.html(this.template({
 				today: self.date
 			}));
-
-			return this;
+                        return this;
 		}
 
 	});
