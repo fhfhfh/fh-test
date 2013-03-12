364ee7f62745b1f14d894d343bab09dabf2e249c
Ashish Bhargava <ashish.bhargava@mindstix.com>
2013-03-12 15:22:09 +0530

    dygraph plugin

--- client/default/app/views/main/calendar/Insights.js
+++ client/default/app/views/main/calendar/Insights.js
@@ -51,70 +51,18 @@ define(['jquery',
                 Backbone.trigger('notify', 'No Information available');
             },
             insightOptions : function(){
-//                alert("Here");
-//                
-//                // example 1 - two basic series
-//$.plot(
-// $("#insight_graph"),
-// [
-//   {
-//     label: "Series 1",
-//     data: [ [0, 0], [1, 1], [2, 1], [3, 2] ],
-//     lines: {show: true},
-//     points: {show: true}
-//   },
-//   {
-//     label: "Series 2",
-//     data: [ [0, 3], [1, 5], [2, 8], [3, 13] ],
-//     lines: {show: true},
-//     points: {show: true}   
-//   }
-// ]
-//);
-//                
+        g = new Dygraph(
 
- var chart1 = new Highcharts.Chart({
-        chart: {
-            renderTo: 'insight_graph',
-            type: 'line'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
-    });
+    // containing div
+    document.getElementById("insight_graph"),
 
-                
-                
-                
-//                var d1 = [];
-//                for (var i = 0; i < 14; i += 0.5)
-//                    d1.push([i, Math.sin(i)]);
-//
-//                var d2 = [[3,12], [8,"2010/11/05"], [5,"2010/11/07"], [13,"2010/11/01"]];
-//
-//                // a null signifies separate line segments
-//                var d3 = [[0, 12], [7, 12], [7, 2.5], [12, 2.5]];
-//    
-//                $.plot($("#insight_graph"), [ d1, d2, d3 ], {
-//                    canvas: false
-//                 
-//                });
-//                        Backbone.trigger('notify', 'No Information available');
+    // CSV or path to a CSV file.
+    "Date,Temperature\n" +
+    "2008-05-07,75\n" +
+    "2008-05-08,70\n" +
+    "2008-05-09,80\n"
+
+  );    
             },
                 
             renderIns: function(){
