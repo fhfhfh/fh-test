79bdfa8a72c8da5e42f9f93cc51fc5be0c5a2b0f
Ashish Bhargava <ashish.bhargava@mindstix.com>
2013-05-09 14:46:32 +0530

    keybord remove on hitting return in lib

--- client/default/app/views/main/library/MyMedia.js
+++ client/default/app/views/main/library/MyMedia.js
@@ -3,79 +3,79 @@
 
 --------------------*/
 define(['jquery',
-        'underscore',
-        'backbone',
-        'text!templates/components/MyMedia.html',
-        'text!templates/components/MyMediaFolder.html',
-        'text!templates/components/MyMediaItem.html',
-        'text!templates/components/ReadingRoomRow.html',
-        'collections/Folders',
-        'collections/Library',
+    'underscore',
+    'backbone',
+    'text!templates/components/MyMedia.html',
+    'text!templates/components/MyMediaFolder.html',
+    'text!templates/components/MyMediaItem.html',
+    'text!templates/components/ReadingRoomRow.html',
+    'collections/Folders',
+    'collections/Library',
 ], function($, _, Backbone, tpl, folderTpl, itemTpl, rowTpl, folderStore, libStore) {
 
-	return Backbone.View.extend({
+    return Backbone.View.extend({
 
-		// Backbone specific attributes
-		tagName		: 'section',
-	    id			: 'myMedia',
-	    events		: {
-	    	'click .cabinetFolder'	: 'displayFolder',
-	    	'click .cabinetItem'	: 'displayFile',
-	    	'click #changeView' 	: 'changeView',
-	    	'keyup #search'			: 'searchItems',
-	    	'click #item'			: 'displayFile'
-	    },
-	    template	: _.template(tpl),
-	    folderTpl 	: _.template(folderTpl),
-	    itemTpl 	: _.template(itemTpl),
-	    rowTpl 	 	: _.template(rowTpl),
+        // Backbone specific attributes
+        tagName		: 'section',
+        id			: 'myMedia',
+        events		: {
+            'click .cabinetFolder'	: 'displayFolder',
+            'click .cabinetItem'	: 'displayFile',
+            'click #changeView' 	: 'changeView',
+            'keyup #search'			: 'searchItems',
+            'click #item'			: 'displayFile'
+        },
+        template	: _.template(tpl),
+        folderTpl 	: _.template(folderTpl),
+        itemTpl 	: _.template(itemTpl),
+        rowTpl 	 	: _.template(rowTpl),
 
 
-		initialize : function(){
-			_.bindAll(this);
-			this.folders  = folderStore.models;
-			this.libStore = libStore.models;
+        initialize : function(){
+            _.bindAll(this);
+            this.folders  = folderStore.models;
+            this.libStore = libStore.models;
 
-			// force page refresh when objects added to collections
-			folderStore.on('add', this.render);
-			libStore.on('add', this.render);
-			folderStore.on('remove', this.render);
-			libStore.on('remove', this.render);
-		},
+            // force page refresh when objects added to collections
+            folderStore.on('add', this.render);
+            libStore.on('add', this.render);
+            folderStore.on('remove', this.render);
+            libStore.on('remove', this.render);
+        },
 
-		render: function(){
-			var folders = this.populateFolders();
-			var items = this.populateItems();
-			var list = this.populateList();
-			this.$el.html(this.template({folders:folders, items: items, list:list}));
-			this.thumbnailUpdate();
+        render: function(){
+            var folders = this.populateFolders();
+            var items = this.populateItems();
+            var list = this.populateList();
+            this.$el.html(this.template({folders:folders, items: items, list:list}));
+            this.thumbnailUpdate();
 			
-			this.bodyScroll = new iScroll(this.$('#cabinetBody')[0],{
-				bounceLock	: true,
-				bounce 		: false,
-				vScrollbar 	: false
-			});
-			this.headerScroll = new iScroll(this.$('#cabinetHeader')[0],{
-				hScroll 	: true,
-				vScroll 	: false,
-				hScrollbar 	: false,
-				bounceLock 	: true,
-				bounce 		: false
-			});
-			this.refreshScroll();
+            this.bodyScroll = new iScroll(this.$('#cabinetBody')[0],{
+                bounceLock	: true,
+                bounce 		: false,
+                vScrollbar 	: false
+            });
+            this.headerScroll = new iScroll(this.$('#cabinetHeader')[0],{
+                hScroll 	: true,
+                vScroll 	: false,
+                hScrollbar 	: false,
+                bounceLock 	: true,
+                bounce 		: false
+            });
+            this.refreshScroll();
 			
-			return this;
-		},
+            return this;
+        },
 
-		refreshScroll: function(){
-			var self = this;
-			setTimeout(function(){
-				self.bodyScroll.refresh();	
-				self.headerScroll.refresh();
-			}, 100);
-		},
+        refreshScroll: function(){
+            var self = this;
+            setTimeout(function(){
+                self.bodyScroll.refresh();	
+                self.headerScroll.refresh();
+            }, 100);
+        },
 
-		populateFolders: function(){
+        populateFolders: function(){
             var folders = this.folders;
             var tpl = this.folderTpl;
             var str = '';
@@ -95,155 +95,159 @@ define(['jquery',
                 var item = items[i].attributes;
 
                 if(!item.folders){
-                	item.folders = '23'	;
+                    item.folders = '23'	;
                 }
 
                 str+=tpl({
-                	id		:item.id, 
-                	shortTitle	:item.shortTitle,
-                	title	:item.title,
-                	type	: 'video',
-                	folders	: item.folders
+                    id		:item.id, 
+                    shortTitle	:item.shortTitle,
+                    title	:item.title,
+                    type	: 'video',
+                    folders	: item.folders
                 });
             }
             return str;
         },
 
         populateList: function(){
-        	var folders = this.folders;
-        	var items = libStore.models;
-        	var tpl = this.rowTpl;
-        	var str = '', i, j;
-        	var src = {
-				video: "img/library/PlayWhiteSmall.png",
-				article: "img/library/ArticleWhiteSmall.png",
-				web: "img/library/GlobeWhiteSmall.png",
-			};
+            var folders = this.folders;
+            var items = libStore.models;
+            var tpl = this.rowTpl;
+            var str = '', i, j;
+            var src = {
+                video: "img/library/PlayWhiteSmall.png",
+                article: "img/library/ArticleWhiteSmall.png",
+                web: "img/library/GlobeWhiteSmall.png",
+            };
 
-        	for(i=0; i<folders.length; i++){
-        		var item = folders[i].attributes;
-        		str += '<tr id="folder"><td>'+item.name+'</td><td></td><td></td></tr>';
+            for(i=0; i<folders.length; i++){
+                var item = folders[i].attributes;
+                str += '<tr id="folder"><td>'+item.name+'</td><td></td><td></td></tr>';
 
-        		for(j=0;j<items.length;j++){
-        			var asset = items[j].attributes;
-        			if(asset.folders.indexOf(item.id) >= 0){
-        				str+= tpl({
-        					id: asset.id,
-        					name: asset.title,
-        					type: 'video',
-        					date: 'Yesterday',
-        					src: src.video
-        				});
-        			}
-        		}
-        	}
-        	return str;
+                for(j=0;j<items.length;j++){
+                    var asset = items[j].attributes;
+                    if(asset.folders.indexOf(item.id) >= 0){
+                        str+= tpl({
+                            id: asset.id,
+                            name: asset.title,
+                            type: 'video',
+                            date: 'Yesterday',
+                            src: src.video
+                        });
+                    }
+                }
+            }
+            return str;
         },
 
         thumbnailUpdate: function(){
-        	var self=this,i;
-        	var items = this.$('.cabinetItem');
+            var self=this,i;
+            var items = this.$('.cabinetItem');
 
-        	for(i=0;i<items.length;i++){
-        		var item = this.$(items[i]);
-        		if(item.attr('data-id')){
-        			var model =libStore.get(item.attr('data-id'));
-        			item.find('.thumbnail').css({
-        				'background':'url('+model.attributes.imgUrl+') no-repeat',
-        				'background-size':'110px 70px',
-        				'background-position':'8px 1px'
-        			});
-        		}
-        	}
+            for(i=0;i<items.length;i++){
+                var item = this.$(items[i]);
+                if(item.attr('data-id')){
+                    var model =libStore.get(item.attr('data-id'));
+                    item.find('.thumbnail').css({
+                        'background':'url('+model.attributes.imgUrl+') no-repeat',
+                        'background-size':'110px 70px',
+                        'background-position':'8px 1px'
+                    });
+                }
+            }
         },
 
-		displayFolder: function(e){
-			var target 	= e.currentTarget;
-			var title 	= $(target).text();
-			var id 		= $(target).attr('id');
+        displayFolder: function(e){
+            var target 	= e.currentTarget;
+            var title 	= $(target).text();
+            var id 		= $(target).attr('id');
 			
-			$('.cabinetFolder').removeClass('selected');
-			$('.cabinetFolder .folderImg').attr('src', 'img/library/AllBoxes@2x.png');
-			$(target).addClass('selected');
-			$(target).find('.folderImg').attr('src', 'img/library/OpenBox@2x.png');
+            $('.cabinetFolder').removeClass('selected');
+            $('.cabinetFolder .folderImg').attr('src', 'img/library/AllBoxes@2x.png');
+            $(target).addClass('selected');
+            $(target).find('.folderImg').attr('src', 'img/library/OpenBox@2x.png');
 
-			if(id == 0){
-				$('.cabinetItem').show();
-				this.bodyScroll.refresh();
-				return;
-			}
+            if(id == 0){
+                $('.cabinetItem').show();
+                this.bodyScroll.refresh();
+                return;
+            }
 
-			var items = $('.cabinetItem');
-			items.hide();
+            var items = $('.cabinetItem');
+            items.hide();
 
-			for(var i=0; i<items.length; i++){
-				var item = items[i];
-				var folders = $(item).attr('folder') || '';
+            for(var i=0; i<items.length; i++){
+                var item = items[i];
+                var folders = $(item).attr('folder') || '';
 
-				if(folders.indexOf(id) >= 0){
-					$(item).show();
-				}
-			}
-			this.bodyScroll.refresh();
-		},
+                if(folders.indexOf(id) >= 0){
+                    $(item).show();
+                }
+            }
+            this.bodyScroll.refresh();
+        },
 
-		displayFile: function(e){
-			var target 	= e.currentTarget;
-			var title 	= $(target).text();
-			var id 		= $(target).attr('data-id');
-			var type 	= $(target).attr('type');
-			var model 	= libStore.get(id);
+        displayFile: function(e){
+            var target 	= e.currentTarget;
+            var title 	= $(target).text();
+            var id 		= $(target).attr('data-id');
+            var type 	= $(target).attr('type');
+            var model 	= libStore.get(id);
 
-			if(type == 'video'){
-				localStorage.setItem('tempVid', JSON.stringify(model));
-		    	Backbone.history.navigate('video', true, true);
-			}
-		},
+            if(type == 'video'){
+                localStorage.setItem('tempVid', JSON.stringify(model));
+                Backbone.history.navigate('video', true, true);
+            }
+        },
 
-		changeView: function(e){
-			var target = e.currentTarget;
-			var name = $(target).text();
+        changeView: function(e){
+            var target = e.currentTarget;
+            var name = $(target).text();
 
-			if(name == 'Shelf View'){
-				$(target).text('List View');
-				$('#cabinet').hide();
-				$('#listView').show();
-			}
-			else {
-				$(target).text('Shelf View');
-				$('#cabinet').show();
-				$('#listView').hide();
-			}
-		},
+            if(name == 'Shelf View'){
+                $(target).text('List View');
+                $('#cabinet').hide();
+                $('#listView').show();
+            }
+            else {
+                $(target).text('Shelf View');
+                $('#cabinet').show();
+                $('#listView').hide();
+            }
+        },
 
-		searchItems: function(e){
-			var target = e.currentTarget;
-			var text = $(target).val().toLowerCase();
-			var i,j;
+        searchItems: function(e){
+            var target = e.currentTarget;
+            var text = $(target).val().toLowerCase();
+            var i,j;
+            if(e.which == 13){
+                $(target).blur();
+            }
 
-			// search cabinet
-			var items = $('.cabinetItem');
-			items.show();
-			for(i=0; i<items.length;i++){
-				var item = $(items[i]);
-				var title = item.find('p').attr('title').toLowerCase();
-				if(title.indexOf(text) == -1){
-					console.log(text, title);
-					item.hide();
-				}
-			}
 
-			// search list
-			var items2 = $('tr#item');
-			items2.show();
-			for(i=0; i<items2.length;i++){
-				var item = $(items2[i]);
-				var title = item.find('#title').text().toLowerCase();
-				if(title.indexOf(text) == -1){
-					item.hide();
-				}
-			}
-		}
+            // search cabinet
+            var items = $('.cabinetItem');
+            items.show();
+            for(i=0; i<items.length;i++){
+                var item = $(items[i]);
+                var title = item.find('p').attr('title').toLowerCase();
+                if(title.indexOf(text) == -1){
+                    console.log(text, title);
+                    item.hide();
+                }
+            }
 
-	});
+            // search list
+            var items2 = $('tr#item');
+            items2.show();
+            for(i=0; i<items2.length;i++){
+                var item = $(items2[i]);
+                var title = item.find('#title').text().toLowerCase();
+                if(title.indexOf(text) == -1){
+                    item.hide();
+                }
+            }
+        }
+
+    });
 });
