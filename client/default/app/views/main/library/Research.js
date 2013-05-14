41f66ff9446888004e5ac6a0e4055fedcd4a732f
Ashish Bhargava <ashish.bhargava@mindstix.com>
2013-05-14 19:35:51 +0530

    research desk welcome screen started

--- client/default/app/views/main/library/Research.js
+++ client/default/app/views/main/library/Research.js
@@ -3,31 +3,37 @@
 
 --------------------*/
 define(['jquery',
-        'underscore',
-        'backbone',
-        'text!templates/components/Research.html',
-], function($, _, Backbone, tpl) {
+    'underscore',
+    'backbone',
+    'text!templates/components/Research.html',
+    ], function($, _, Backbone, tpl) {
 
-	return Backbone.View.extend({
+        return Backbone.View.extend({
 
-		// Backbone specific attributes
-		tagName		: 'section',
-	    id			: 'research',
-	    events		: {
-	    },
-	    template	: _.template(tpl),
+            // Backbone specific attributes
+            tagName		: 'section',
+            id			: 'research',
+            events		: {
+            },
+            template	: _.template(tpl),
 
 
-		initialize : function(){
-			_.bindAll(this);
-		},
+            initialize : function(){
+                _.bindAll(this);
+            },
 
-		render: function(){
-			var self = this;
+            render: function(){
+                var self = this;
 
-			this.$el.html(this.template());
-			return this;
-		}
+                this.$el.html(this.template());
+                this.bodyScroll = new iScroll(this.$('#research-desk')[0],{
+                    bounceLock	: true,
+                    bounce 		: false,
+                    vScrollbar 	: false
+                });
+                this.refreshScroll();
+                return this;
+            }
 
-	});
-});
+        });
+    });
