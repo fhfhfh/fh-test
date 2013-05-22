b16f9cc3b9a0ede418132bf08cc5a6199ee38ad8
Ashish Bhargava <ashish.bhargava@mindstix.com>
2013-05-22 18:55:19 +0530

    information screen added in research desk

--- client/default/app/views/main/library/Research.js
+++ client/default/app/views/main/library/Research.js
@@ -8,7 +8,9 @@ define(['jquery',
     'text!templates/components/Omnipedial.html',
     'text!templates/components/Research.html',
     'text!templates/components/OmnipediaDetails.html',
-    ], function($, _, Backbone, omnipidiaTpl,tpl,omniDetailsTpl) {
+    'text!templates/components/Information.html',
+    'text!templates/components/InformationDetails.html',
+    ], function($, _, Backbone, omnipidiaTpl,tpl,omniDetailsTpl,informationTpl,infoDetailsTpl) {
 
         return Backbone.View.extend({
 
@@ -17,9 +19,10 @@ define(['jquery',
             id			: 'research',
             events		: {
                 'click #menu1' : 'showOmnipidia',
+                'click #menu2' : 'showInformation',
                 'click #quitBtn' : 'render',
-//                'click #clrBtn' :'loadList'
-                'click #omniListArea #omniDetails .boxEntry' : 'showOmnipidiaDetails'
+                'click #omniListArea #omniDetails .boxEntry' : 'showOmnipidiaDetails',
+                'click #infoListArea #infoDetails .boxEntry' : 'showInformationDetails'
  
             },
             template	: _.template(tpl),
@@ -57,7 +60,40 @@ define(['jquery',
                     description: "info",
                     src: imgSrc
                 }));
-                this.loadList();
+                this.loadListOmni();
+            },
+            
+            showInformation: function(e){
+                var target = e.currentTarget;
+                var page = $(this.el);
+
+                var details = _.template(informationTpl);
+                var title = $(target).find('h1').text() || 'Untitled';
+                // var info = $(target).attr('info');
+                var imgSrc = $(target).find('img').attr('src');
+
+                this.$el.html(details({
+                    title: title,
+                    description: "info",
+                    src: imgSrc
+                }));
+                this.loadListInfo();
+            },
+            
+            showInformationDetails: function(e){
+                var target = e.currentTarget;
+                var page = $(this.el);
+
+                var details = _.template(infoDetailsTpl);
+                var title = $(target).find('h1').text() || 'Untitled';
+                // var info = $(target).attr('info');
+                var imgSrc = $(target).find('img').attr('src');
+
+                this.$el.html(details({
+                    title: title,
+                    description: "info",
+                    src: imgSrc
+                }));
             },
                 
                 
@@ -76,8 +112,9 @@ define(['jquery',
                     src: imgSrc
                 }));
             },
+            
                 
-            loadList : function(){
+            loadListOmni : function(){
                 var flag = 0;
                 var name = new Array("A-fib","AAA","Aarskog syndrome","B-fib","BBB","C-fib");
                 name.sort();
@@ -105,6 +142,40 @@ define(['jquery',
                         var html = "<div class='boxEntry'  id='"+temp+"''><span id='name'> "+temp+"</span>"+
                         "<span id='about'>" + "</span></div>";
                         $('#omniDetails').append(html);
+                        flag=1;
+                    }
+                        
+                }
+            },
+            
+            loadListInfo : function(){
+                var flag = 0;
+                var name = new Array("A-fib","AAA","Aarskog syndrome","B-fib","BBB","C-fib");
+                name.sort();
+                for(var i=0;i<name.length;i++){
+                    
+                    var temp = name[i];
+                    if(i==0){
+                    {
+                        $('#infoDetails').append("<div id='infoListHead'>"+temp.charAt(0)+"</div>");
+                    }
+                    }
+                    if(!i==0)
+                        if(temp.charAt(0)!=name[i-1].charAt(0))
+                        {
+                            $('#infoDetails').append("<div id='infoListHead'>"+temp.charAt(0)+"</div>");
+                        }
+                    if(flag == 1){
+                        var html = "<div class='boxEntry' style='background-color:#ebebeb' id='"+temp+"''><span id='name'> "+temp+"</span>"+
+                        "<span id='about'>" + "</span></div>";
+                        $('#infoDetails').append(html);
+                        flag=0;
+                    }
+                    else
+                    {
+                        var html = "<div class='boxEntry'  id='"+temp+"''><span id='name'> "+temp+"</span>"+
+                        "<span id='about'>" + "</span></div>";
+                        $('#infoDetails').append(html);
                         flag=1;
                     }
                         
