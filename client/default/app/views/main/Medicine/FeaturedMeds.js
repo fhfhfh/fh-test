f8b1bc6c982e146fe8f68c6ad1e5cbf9a829cd94
Brid Mackey <brid.mackey@feedhenry.com>
2013-05-24 16:56:37 +0100

    addition of view option pop up on featured medicine screen

--- client/default/app/views/main/Medicine/FeaturedMeds.js
+++ client/default/app/views/main/Medicine/FeaturedMeds.js
@@ -7,23 +7,44 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'text!templates/popups/FilterView.html',
     'text!templates/components/FeaturedMeds.html'
-], function($, _, Backbone, tpl) {
+], function($, _, Backbone, filterTpl, tpl) {
     return Backbone.View.extend({
         tagName: 'section',
         id: 'featuredMedsScreen',
-        template: _.template(tpl),
-        events: {
 
+        events  : {
+            'click #viewOpt' : 'showFilter',
         },
+
+        template: _.template(tpl),
+        filterTpl : _.template(filterTpl),
 
         initialize: function() {
             _.bindAll(this);
+            this.$el.html(this.template());
         },
 
         render: function() {
-            this.$el.html(this.template());
             return this;
+        },
+
+        showFilter: function(){
+            var self = this;
+            var body = "<label data-id='viewMedCab' class='checked'>View in Medicine Cabinet<img src='img/healthHub/CheckmarkOrange.png'/></label>"+
+                    "<label data-id='viewMedList' class='checked'>View Medicines as a List<img src='img/healthHub/CheckmarkOrange.png'/></label>"+
+                    "<label data-id='showMed' class='checked'>Show Medication<img src='img/healthHub/CheckmarkOrange.png'/></label>"+
+                    "<label data-id='showSupplements' class='checked'>Show Supplements<img src='img/healthHub/CheckmarkOrange.png'/></label>"+
+                    "<label data-id='showMedDevices' class='checked'>Show Devices<img src='img/healthHub/CheckmarkOrange.png'/></label>";
+            
+            if($('div #filterView').length > 0){
+                $('div #filterView').toggle();
+                return;
+            }
+            else {
+                $('#featuredMeds').append(self.filterTpl({body: body})); 
+            }
         },
 
     });
